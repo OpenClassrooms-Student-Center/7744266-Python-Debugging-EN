cc677921b92d6b4d8280d61f26aa6f642ff963e6
Amelia Crowther <amelia@lambertlabs.com>
2022-06-10 14:46:03 +0100

    made alterations to error examples

--- p2c1_exceptions.py
+++ p2c1_exceptions.py
@@ -1,42 +1,41 @@
 # AttributeError
 
-#import random
-#print("The value generated is:", random.randint(0, 10))
+# import random
+# print("The value generated is:", random.randinte(0, 10))
 
 # KeyError
 
-#projectDAPythonPath = {"Project 1": "Take charge of your Python training program", 
-#                "Project 2": "Use Python basics for market analysis",
-#                "Project 3": "Design a Python application suited to a client's needs}
-#print(projectDAPythonPath["Project 4"])
+# projectDAPythonPath = {
+#   "Project 1": "Take charge of your Python training program",
+#   "Project 2": "Use Python basics for market analysis",
+#   "Project 3": "Design a Python application suited to a client's needs"
+# }
+# print(projectDAPythonPath["Project 4"])
 
 # IndexError
 
-#list = [10, 20, 30, 40, 50, 60]
-#print(list[7])
-#print(list({"1" : "A", "2" : "B"}))
+# list = [10, 20, 30, 40, 50, 60]
+# print(list[0])
 
 # NameError
 
-#print(recursive_factorial(42))
+# print(recursive_factorial(42))
 
 # SyntaxError
 
-#if True
+# if True
 #    print "It's true
 
 # TypeError
 
-#print(40 + "2")
-#print(list(42))
+# print(40 + "2")
+# print(list(42))
 
 # ValueError
 
-#print(int("5"))
-#print(float("5.0"))
-#print(int("five"))
-#print(int("five"))
-#list({"1" : "A", "2" : "B"})
+# print(int("5"))
+# print(float("5.0"))
+# print(int("green"))
 
 # FileNotFound
 
