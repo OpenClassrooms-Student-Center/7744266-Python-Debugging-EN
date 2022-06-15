95a1fe338352038c6286035ee2785968db373928
Amelia Crowther <amelia@lambertlabs.com>
2022-06-15 15:06:29 +0100

    year of birth not date of birth

--- exercise_solution_p3c1.py
+++ exercise_solution_p3c1.py
@@ -1,15 +1,15 @@
 from datetime import *
 
 try:
-    date_of_birth = int(input("What is your date of birth?"))
-    age = int(datetime.now().strftime("%Y")) - date_of_birth
+    year_of_birth = int(input("What is your year of birth?"))
+    age = int(datetime.now().strftime("%Y")) - year_of_birth
 except:
     print("Oops, there's an error in the code.")
     while True:
         try:
-            date_of_birth = int(input("Enter your date of birth, using only figures."))
-            if type(date_of_birth) == int:
-                age = int(datetime.now().strftime("%Y")) - int(date_of_birth)
+            year_of_birth = int(input("Enter your year of birth, using only figures."))
+            if type(year_of_birth) == int:
+                age = int(datetime.now().strftime("%Y")) - int(year_of_birth)
                 print(f"You are {age} years old.")
                 break
         except:
