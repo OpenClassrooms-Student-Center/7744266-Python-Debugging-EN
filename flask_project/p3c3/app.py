05fd8bfb186840ad0520bc5df7a2cc52539950d9
Amelia Crowther <amelia@lambertlabs.com>
2022-07-12 19:09:05 +0100

    added typo fro screencast

--- flask_project/p3c3/app.py
+++ flask_project/p3c3/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    return render_template("indexe.html")
 
 @app.route("/forbidden")
 def forbidden():
